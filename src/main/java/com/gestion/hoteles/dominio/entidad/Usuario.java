9e8e39f9b4382e152ad62d66dacac9ffeae130bf
Pablo Abal Diez <112098403+pabloabaldiez@users.noreply.github.com>
2024-02-19 18:27:08 -0300

    entidad usuario

--- src/main/java/com/gestion/hoteles/dominio/entidad/Usuario.java
+++ src/main/java/com/gestion/hoteles/dominio/entidad/Usuario.java
@@ -1,2 +1,32 @@
-package com.gestion.hoteles.dominio.entidad;public class Usuario {
+package com.gestion.hoteles.dominio.entidad;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity(name= "usuario")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "nombre")
+    String nombre;
+
+    @Column(name = "apellido")
+    String apellido;
+
+    @Column(name = "rol")
+    String rol;
+
 }
