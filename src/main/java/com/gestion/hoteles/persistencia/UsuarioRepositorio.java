ca3d57823083483966c39d8b7f84c3e1ac125ed1
Pablo Abal Diez <112098403+pabloabaldiez@users.noreply.github.com>
2024-02-21 16:51:46 -0300

    completando repositorio

--- src/main/java/com/gestion/hoteles/persistencia/UsuarioRepositorio.java
+++ src/main/java/com/gestion/hoteles/persistencia/UsuarioRepositorio.java
@@ -4,8 +4,13 @@ import com.gestion.hoteles.dominio.entidad.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario,Integer>{
+    Optional <Usuario> findUsuarioPorId(int id);
+    Optional <List<Usuario>> findListaUsuarios();
 
 }
