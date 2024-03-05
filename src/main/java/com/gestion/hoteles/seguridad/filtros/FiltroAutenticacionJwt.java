a60be4b70baebc024a606d521766532858dc14e1
Pablo Abal Diez <112098403+pabloabaldiez@users.noreply.github.com>
2024-03-04 21:22:58 -0300

    primer filtro

---
+++ src/main/java/com/gestion/hoteles/seguridad/filtros/FiltroAutenticacionJwt.java
@@ -0,0 +1,7 @@
+package com.gestion.hoteles.seguridad.filtros;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class FiltroAutenticacionJwt extends UsernamePasswordAuthenticationFilter {
+}
